de85e107568db38f5c8524964d9626d8b5b153ae
woo88 <woo88@kaist.ac.kr>
2015-12-01 20:50:13 +0900

    test commit.

---
+++ Kmap/src/main/java/kr/ac/kaist/kmap/App.java
@@ -0,0 +1,11 @@
+package kr.ac.kaist.kmap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
