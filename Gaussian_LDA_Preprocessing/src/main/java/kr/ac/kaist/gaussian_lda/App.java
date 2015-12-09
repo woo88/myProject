d8a65e16294d0b74ef4257f34b330a9e4eea57cf
woo88 <woo88@kaist.ac.kr>
2015-12-10 00:59:33 +0900

    test

--- Gaussian_LDA_Preprocessing/src/main/java/kr/ac/kaist/gaussian_lda/App.java
+++ Gaussian_LDA_Preprocessing/src/main/java/kr/ac/kaist/gaussian_lda/App.java
@@ -1,12 +1,15 @@
 package kr.ac.kaist.gaussian_lda;
 
+import java.io.File;
+
 /**
  * Created by Woo on 2015. 12. 10..
  */
 public class App {
-    private static String inputCorpus= "/home";
+    private static String inputCorpus= "data/word2vec_sentence.txt";
 
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        File f = new File(inputCorpus);
+        System.out.println(f.isFile());
     }
 }
