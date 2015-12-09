bfb01331ec0f5fcefd8326bb103c3830cfbe929d
woo88 <woo88@kaist.ac.kr>
2015-12-10 00:48:32 +0900

    init commit for preprocessing Gaussian LDA

---
+++ Gaussian_LDA_Preprocessing/src/main/java/ac/kr/kaist/gaussian_lda/App.java
@@ -0,0 +1,13 @@
+package ac.kr.kaist.gaussian_lda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    private static String inputCorpus= "/home";
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
